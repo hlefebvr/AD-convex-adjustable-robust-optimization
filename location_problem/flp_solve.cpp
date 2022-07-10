137c69c8bc9adba14544511363eb16ad8eef8cbb
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-07-10 15:25:18 +0200

    add instances

--- location_problem/flp_solve.cpp
+++ location_problem/flp_solve.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "location_problem/instance/flp_RandomInstance.h"
-#include "location_problem/separation/flp_SeparationProblem.h"
-#include "location_problem/master/flp_MasterProblem.h"
-#include "location_problem/callback_cutting_plane/flp_CuttingPlaneCallback.h"
-#include "location_problem/instance/flp_InstanceFromFile.h"
+#include "instance/flp_RandomInstance.h"
+#include "separation/flp_SeparationProblem.h"
+#include "master/flp_MasterProblem.h"
+#include "callback_cutting_plane/flp_CuttingPlaneCallback.h"
+#include "instance/flp_InstanceFromFile.h"
 
 using namespace flp;
 
@@ -67,53 +67,28 @@ void solve_and_report(const flp::Instance& t_instance, double t_tolerance = 1e-8
     << std::endl;
 }
 
-int main() {
+int main(int t_argc, const char** t_argv) {
 
-    using namespace flp;
+    if (t_argc != 4) { throw std::runtime_error("Expected parameters: <INSTANCE_FILE> <DEVIATION> <ALG=0,1,2>"); }
 
-    const double deviation = .15;
+    const std::string path = t_argv[1];
+    const double deviation = std::atof(t_argv[2]);
+    const auto algorithm = (Algorithm) std::atoi(t_argv[3]);
 
-    const std::vector<std::pair<unsigned int, unsigned int>> configs = {
-            { 4, 8 },
-            { 5, 20 },
-            { 10, 20 },
-            { 10, 30 },
-            { 15, 30 },
-            { 15, 40 },
-            { 20, 40 },
-            { 20, 50 },
-            { 25, 50 },
-            { 25, 60 },
-            { 30, 60 },
-            { 30, 70 }
-    };
+    InstanceFromFile instance(path);
 
-    for (const auto& [n_sites, n_clients] : configs) {
+    for (unsigned int g = 0, G = instance.n_clients(); g <= G; g += 1) {
 
-        for (unsigned int k = 0 ; k < 5 ; k += 1) {
+        instance.set_robust_parameters(g, deviation);
 
-            RandomInstance instance(n_sites, n_clients, 1.4);
-            //InstanceFromFile instance("instance.txt");
-
-            for (unsigned int Gamma = 0; Gamma <= n_clients; Gamma += 1) {
-
-                instance.set_robust_parameters(Gamma, deviation);
-
-                try {
-                    solve_and_report<AddCuts>(instance);
-                    solve_and_report<AddCutsInCallback>(instance);
-                    solve_and_report<AddScenarioVariables>(instance);
-                } catch (const GRBException& err) {
-                    std::cout << err.getMessage() << std::endl;
-                    __throw_exception_again;
-                }
-            }
-
-            throw 10;
-
+        switch (algorithm) {
+            case AddScenarioVariables: solve_and_report<AddScenarioVariables>(instance);    break;
+            case AddCuts: solve_and_report<AddCuts>(instance);                              break;
+            case AddCutsInCallback: solve_and_report<AddCutsInCallback>(instance);          break;
         }
 
     }
+
 
     return 0;
 }
