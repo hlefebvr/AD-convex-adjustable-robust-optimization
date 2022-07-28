f5bee7ece9c7ea11fdbb8d186fa678d53b871cc2
Henri Lefebvre <hri.lefebvre@gmail.com>
2022-07-28 14:55:50 +0200

    add instances for RAP

--- resource_allocation/rap_generate.cpp
+++ resource_allocation/rap_generate.cpp
@@ -3,15 +3,48 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include "instance/rap_RandomInstance.h"
 
 using namespace rap;
 
-int main(int argc, const char**) {
+void generate_instance(const std::string& t_folder, unsigned int t_n_resources, unsigned int t_n_clients, unsigned int t_k) {
+    RandomInstance instance(t_n_resources, t_n_clients);
 
-    RandomInstance instance(10, 20);
+    std::stringstream filename;
+    filename << "rap_instance_r" << t_n_resources
+             << "_c" << t_n_clients
+             << "__" << t_k
+             << ".txt"
+            ;
 
-    std::cout << instance << std::endl;
+    std::ofstream file(t_folder + "/" + filename.str());
+    if (!file.is_open()) { throw std::runtime_error("Could not open destination file."); }
+    file << instance;
+    file.close();
+
+}
+
+int main(int t_argc, const char** t_argv) {
+
+    if (t_argc != 2) { throw std::runtime_error("Expected parameters: <DESTINATION_FOLDER>"); }
+    const std::string path = t_argv[1];
+
+    const std::vector<std::pair<unsigned int, unsigned int>> sizes = {
+            { 10, 20 },
+            { 10, 30 },
+            { 15, 30 },
+            { 15, 40 },
+            { 20, 40 },
+            { 20, 50 },
+    };
+
+    for (const auto& [n_site, n_clients] : sizes) {
+        for (unsigned int k = 0 ; k < 5 ; k += 1) {
+            generate_instance(path, n_site, n_clients, k);
+        }
+    }
 
     return 0;
 }
---
+++ resource_allocation/rap_solve.cpp
@@ -0,0 +1,14 @@
+//
+// Created by henri on 28/07/22.
+//
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "instance/rap_InstanceFromFile.h"
+
+using namespace rap;
+
+int main(int t_argc, const char** t_argv) {
+
+    return 0;
+}
