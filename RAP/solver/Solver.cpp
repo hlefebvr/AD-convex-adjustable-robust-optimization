9ab449fef0dd3381d88991a61ecf9ea6d3f89fa1
Henri Lefebvre <hri.lefebvre@gmail.com>
2025-10-01 15:25:25 +0200

    add larger instances

--- RAP/solver/Solver.cpp
+++ RAP/solver/Solver.cpp
@@ -49,6 +49,7 @@ idol::Solution::Primal RAP::Solver::solve_master_problem(double t_time_limit) {
     if (const auto status = m_master_problem.get_status() ; status != Optimal && status != Feasible ) {
         result.set_status(status);
         result.set_reason(m_master_problem.get_reason());
+        result.set_objective_value(m_master_problem.get_best_bound());
         return result;
     }
 
